9add0cf7a077e7f6b5ee82dafcdae860dacacc28
Francisco <franvillago@gmail.com>
2023-11-14 09:21:46 -0300

    prueba

--- bases/urls.py
+++ bases/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
       path('login/',auth_views.LoginView.as_view(template_name='bases/login.html'), name='login-vw'),
       path('logout/',auth_views.LogoutView.as_view(template_name='bases/login.html'), name='logout-vw'),
       path('sin_privilegios/',HomeSinPrivilegios.as_view(), name='sin_privilegios-vw'),
+      
 ]
